9f110c4aa2c0ccf1add07d761bc18f7ad103e6ac
Nova Mondal <rialmondal@gmail.com>
2023-12-20 13:59:10 -0800

    initial setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+/**@todo */
+export const PROJECT_NAME = "[Project Name]";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>{PROJECT_NAME} Docs</span>,
+	project: {
+		link: "https://github.com/frc-emotion",
+	},
+	docsRepositoryBase: "https://github.com/frc-emotion/docs",
+	footer: {
+		text: PROJECT_NAME + " by FRC Team 2658 Σ-Motion",
+	},
+	editLink: { text: "" },
+	useNextSeoProps() {
+		return {
+			titleTemplate: `%s - ${PROJECT_NAME} Docs`,
+		};
+	},
+	head: (
+		<head>
+			<link rel="icon" href="/favicon.ico" />
+		</head>
+	),
+};
 
-export default config
+export default config;
